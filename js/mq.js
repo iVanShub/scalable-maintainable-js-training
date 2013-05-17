e29c3262cd6bfd042c1aa2188bb6bcf8a49c29a6
iVan Shubyonkin <mail@ivansh.ru>
2013-05-17 10:38:37 +0600

    bootstrap

---
+++ js/mq.js
@@ -0,0 +1,15 @@
+var newModule = (function (window, $) {
+
+    // private vars and methods
+    var bar = 0;
+    
+    return {
+        
+        // public vars and methods
+        foo: function () {
+            // ...
+        }
+    
+    };
+
+})(window, jQuery);
